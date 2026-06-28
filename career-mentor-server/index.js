a18d2de06a32f9d8c690a3ecc7b4c4fd7351041e
Jigyansa_Jena <jigyanajena56@gmail.com>
2026-06-28 13:11:56 +0530

    fix: app issue

--- career-mentor-server/index.js
+++ career-mentor-server/index.js
@@ -31,4 +31,7 @@ app.use('/api/resume', require('./routes/resumeRoutes'));
 app.use('/api/skills', require('./routes/skillRoutes'));
 app.use('/api/interview', require('./routes/interviewRoutes'));
 
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+
 module.exports = app;
